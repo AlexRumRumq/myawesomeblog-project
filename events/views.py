39a2e042d35aab969dd0f71ca95a1df9100b7ded
Alex <race5755@gmail.com>
2021-04-06 22:12:16 +0300

    Finished

--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.db import models
+from .models import Event
+
 
 # Create your views here.
+def home(request):
+    events = Event.objects
+    return render(request, 'events/home.html', {'events': events})
